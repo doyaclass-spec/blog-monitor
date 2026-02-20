8f017ce40aeb02ddd8c628bfed9314b17ceda68e
doyaclass-spec <doyaclass@gmail.com>
2026-02-20 12:06:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import urllib.request
 import urllib.parse
 import xml.etree.ElementTree as ET
